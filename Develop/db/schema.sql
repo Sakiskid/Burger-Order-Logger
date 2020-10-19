9c05b719c4c3675e94080a6ea051a1f678869be7
Sakiskid <tylerdrew.home@gmail.com>
2020-10-19 12:05:20 -0500

    added db schema and seed

---
+++ Develop/db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE burger_db;
+CREATE DATABASE burger_db;
+
+USE burger_db;
+
+CREATE TABLE burgers 
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(40) NOT NULL,
+    devoured BOOLEAN NOT NULL,
+    PRIMARY KEY (id) 
+)
---
+++ Develop/db/seeds.sql
@@ -0,0 +1,8 @@
+USE burger_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Baby you can chive my car', false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Are we gruyere yet', false);
