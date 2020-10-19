fc82f0f01e30662614478d4c5bd7ad5ebeec2b20
Sakiskid <tylerdrew.home@gmail.com>
2020-10-19 12:15:43 -0500

    added app listen, fixed process typo, and fixed entry point in package.json

--- Develop/server.js
+++ Develop/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 
-const PORT = proccess.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.engine('handlebars', exphbs({ defaultLayout: "main "}));
 app.set('view engine', 'handlebars');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.listen(err => {
+    if(err) throw err;
+    console.log("Listening on port " + PORT);
+})
